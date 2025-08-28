18352e2d7f4ad2a87f0bc80c5c6c3bce08a00042
Nariman <aliyevnariman98@gmail.com>
2025-08-28 19:42:03 +0200

    tig-app

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,16 +1,12 @@
-import type { AccessArgs, CollectionConfig } from 'payload'
-import { User } from './Users'
+import type { CollectionConfig } from 'payload'
 
 export const Media: CollectionConfig = {
   slug: 'media',
   access: {
     read: () => true,
-    create: ({ req: { user } }: AccessArgs<any>) =>
-      ['admin', 'user'].includes((user as User)?.role),
-    update: ({ req: { user } }: AccessArgs<any>) =>
-      ['admin', 'user'].includes((user as User)?.role),
-    delete: ({ req: { user } }: AccessArgs<any>) =>
-      ['admin', 'user'].includes((user as User)?.role),
+    create: () => true,
+    update: () => true,
+    delete: () => true,
   },
   upload: {
     disableLocalStorage: true,
